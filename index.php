c4bd5f6edda9657ba08b5ed5da3436759653270a
Matthieu <parismatthieu@yahoo.fr>
2018-09-19 09:49:39 +0200

    modif

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-salut
+<?php
+
+?>
+<html>
+<section style="width: 100%; height: 100%; background-color: red;">
+    <div style="top: 0; display: inline-block;  width: 100%;">
+        <p style="color: white; text-align: center">Bienvenue !</p>
+
+    </div>
+
+</section>
+</html>
