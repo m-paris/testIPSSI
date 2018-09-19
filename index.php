36c89341aeb51f821d5538ed3c97f439b827a097
scoorp29 <scorp31.scorp31@gmail.com>
2018-09-19 11:02:20 +0200

    modif index

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 <section style="width: 100%; height: 100%; background-color: red;">
     <div style="top: 0; display: inline-block;  width: 100%;">
         <p style="color: white; text-align: center">Bienvenue !</p>
+        <p style="color: #000; text-align: center">Bienvenue !</p>
+
     </div>
 
 </section>
