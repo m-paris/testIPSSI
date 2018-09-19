d82f1c97ee0fa1b7996b6053cc9afbeb1fc0805d
scoorp29 <scorp31.scorp31@gmail.com>
2018-09-19 10:15:50 +0200

    Ma branche

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 <section style="width: 100%; height: 100%; background-color: red;">
     <div style="top: 0; display: inline-block;  width: 100%;">
         <p style="color: white; text-align: center">Bienvenue !</p>
-
     </div>
 
 </section>
